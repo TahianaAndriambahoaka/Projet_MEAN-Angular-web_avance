f9228439d3b98fa2aa2ffc86661f616e9c8222b1
Tahiana Andriambahoaka <89937368+TahianaAndriambahoaka@users.noreply.github.com>
2023-01-25 01:24:50 +0300

    modification du header et sidenav | responsable atelier | affichage

--- src/app/responsableAtelier/accueil/accueil.component.ts
+++ src/app/responsableAtelier/accueil/accueil.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { FooterComponent } from 'src/app/footer/footer.component';
 
 @Component({
@@ -8,5 +9,18 @@ import { FooterComponent } from 'src/app/footer/footer.component';
   entryComponents: [FooterComponent]
 })
 export class AccueilComponent {
-
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        let url = event.url;
+        document.getElementById('listevoituresgarage')!.setAttribute("style", "margin-top: 5px; font-size: 1em; width: 100%; height: 3em;");
+        document.getElementById('listevoituresareceptionner')!.setAttribute("style", "margin-top: 5px; font-size: 1em; width: 100%; height: 3em;");
+        if (url === '/responsable-atelier/voitures') {
+            document.getElementById('listevoituresgarage')!.setAttribute("style", "margin-top: 5px; font-size: 1em; width: 100%; height: 3em; background-color: #3f51b5; color: white; box-shadow: 5px 5px 10px #888888;");
+        } else {
+            document.getElementById('listevoituresareceptionner')!.setAttribute("style", "margin-top: 5px; font-size: 1em; width: 100%; height: 3em; background-color: #3f51b5; color: white; box-shadow: 5px 5px 10px #888888;");
+        }
+      }
+    });
+}
 }
