16d1b7059bd6e4e9e616b8413ddaf47b3c764fac
Tahiana Andriambahoaka <89937368+TahianaAndriambahoaka@users.noreply.github.com>
2023-01-25 01:26:49 +0300

    liste des voitures à réceptionner | responsable atelier | affichage

--- src/app/responsableAtelier/reception-voiture/reception-voiture.component.ts
+++ src/app/responsableAtelier/reception-voiture/reception-voiture.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { ListerReparationComponent } from './../lister-reparation/lister-reparation.component';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSort, Sort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+
+const ELEMENT_DATA = [
+  {numero: '9876TBS', marque: 'Ford Raptor', dateDepot: new Date(), reception: ''},
+  {numero: '9877TBS', marque: 'Ford Ranger', dateDepot: new Date(), reception: ''},
+  {numero: '9876TBS', marque: 'Toyota Hilux', dateDepot: new Date(), reception: ''},
+  {numero: '9876TBS', marque: 'Ford Raptor', dateDepot: new Date(), reception: ''},
+  {numero: '9876TBS', marque: 'Ford Raptor', dateDepot: new Date(), reception: ''},
+  {numero: '9876TBS', marque: 'Ford Raptor', dateDepot: new Date(), reception: ''},
+  {numero: '9876TBS', marque: 'Ford Raptor', dateDepot: new Date(), reception: ''},
+  {numero: '9876TBS', marque: 'Ford Raptor', dateDepot: new Date(), reception: ''},
+  {numero: '9876TBS', marque: 'Ford Raptor', dateDepot: new Date(), reception: ''},
+  {numero: '9876TBS', marque: 'Ford Raptor', dateDepot: new Date(), reception: ''},
+  {numero: '9876TBS', marque: 'Ford Raptor', dateDepot: new Date(), reception: ''},
+  {numero: '9876TBS', marque: 'Ford Raptor', dateDepot: new Date(), reception: ''},
+  {numero: '9876TBS', marque: 'Ford Raptor', dateDepot: new Date(), reception: ''},
+  {numero: '9876TBS', marque: 'Ford Raptor', dateDepot: new Date(), reception: ''},
+];
 
 @Component({
   selector: 'app-reception-voiture',
   templateUrl: './reception-voiture.component.html',
   styleUrls: ['./reception-voiture.component.css']
 })
-export class ReceptionVoitureComponent {
+export class ReceptionVoitureComponent implements AfterViewInit {
+  displayedColumns: string[] = ['numero', 'marque', 'dateDepot', 'reception'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+  constructor(private _liveAnnouncer: LiveAnnouncer, public dialog: MatDialog) {}
+
+  @ViewChild(MatSort) sort!: MatSort;
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+
+  announceSortChange(sortState: Sort | any) {
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
+
+  openDialog(numero: string) {
+    this.dialog.open(ListerReparationComponent, {
+      data: {
+        numero: numero
+      }
+    });
+  }
 
 }
