8df33b9022c82bc0daf0f4cc20d07c3812c87c88
Tahiana Andriambahoaka <89937368+TahianaAndriambahoaka@users.noreply.github.com>
2023-01-25 01:31:04 +0300

    liste des voitures dans le garage | responsable atelier | affichage

---
+++ src/app/responsableAtelier/voiture-garage/voiture-garage.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { VoitureGarageComponent } from './voiture-garage.component';
+
+describe('VoitureGarageComponent', () => {
+  let component: VoitureGarageComponent;
+  let fixture: ComponentFixture<VoitureGarageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ VoitureGarageComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(VoitureGarageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/responsableAtelier/voiture-garage/voiture-garage.component.ts
@@ -0,0 +1,48 @@
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { Component, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSort, Sort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+
+const ELEMENT_DATA = [
+  {numero: '9876TBS', marque: 'Ford Raptor', avancement_reparation: '', facture: ''},
+  {numero: '9877TBS', marque: 'Ford Ranger', avancement_reparation: '', facture: ''},
+  {numero: '9876TBS', marque: 'Toyota Hilux', avancement_reparation: '', facture: ''},
+  {numero: '9876TBS', marque: 'Ford Raptor', avancement_reparation: '', facture: ''},
+  {numero: '9876TBS', marque: 'Ford Raptor', avancement_reparation: '', facture: ''},
+  {numero: '9876TBS', marque: 'Ford Raptor', avancement_reparation: '', facture: ''},
+  {numero: '9876TBS', marque: 'Ford Raptor', avancement_reparation: '', facture: ''},
+  {numero: '9876TBS', marque: 'Ford Raptor', avancement_reparation: '', facture: ''},
+  {numero: '9876TBS', marque: 'Ford Raptor', avancement_reparation: '', facture: ''},
+  {numero: '9876TBS', marque: 'Ford Raptor', avancement_reparation: '', facture: ''},
+  {numero: '9876TBS', marque: 'Ford Raptor', avancement_reparation: '', facture: ''},
+  {numero: '9876TBS', marque: 'Ford Raptor', avancement_reparation: '', facture: ''},
+  {numero: '9876TBS', marque: 'Ford Raptor', avancement_reparation: '', facture: ''},
+  {numero: '9876TBS', marque: 'Ford Raptor', avancement_reparation: '', facture: ''},
+];
+
+@Component({
+  selector: 'app-voiture-garage',
+  templateUrl: './voiture-garage.component.html',
+  styleUrls: ['./voiture-garage.component.css']
+})
+export class VoitureGarageComponent {
+  displayedColumns: string[] = ['numero', 'marque', 'avancement_reparation', 'facture'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+  constructor(private _liveAnnouncer: LiveAnnouncer, public dialog: MatDialog) {}
+
+  @ViewChild(MatSort) sort!: MatSort;
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+
+  announceSortChange(sortState: Sort | any) {
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
+
+}
