fa83e60fbfd27e0b85979ba8bbc803c3e0c42a6a
Tahiana Andriambahoaka <89937368+TahianaAndriambahoaka@users.noreply.github.com>
2023-01-30 15:16:00 +0300

    integration | historique des réparations | côté client

--- src/app/client/historique-reparation/historique-reparation.component.ts
+++ src/app/client/historique-reparation/historique-reparation.component.ts
@@ -3,23 +3,46 @@ import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '
 import { MatSort, Sort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { ActivatedRoute } from '@angular/router';
+import { FooterComponent } from 'src/app/footer/footer.component';
+import { HeaderComponent } from '../header/header.component';
 
 const ELEMENT_DATA = [
-  {reparation: 'Réparation ...', date: new Date(), etat_avancement: 'Réparée'},
-  {reparation: 'Réparation ...', date: new Date(), etat_avancement: 'En cours de réparation'},
-  {reparation: 'Réparation ...', date: new Date(), etat_avancement: 'Réparée'},
-  {reparation: 'Réparation ...', date: new Date(), etat_avancement: 'En cours de réparation'},
-  {reparation: 'Réparation ...', date: new Date(), etat_avancement: 'Réparée'},
-  {reparation: 'Réparation ...', date: new Date(), etat_avancement: 'En cours de réparation'},
-  {reparation: 'Réparation ...', date: new Date(), etat_avancement: 'Réparée'},
-  {reparation: 'Réparation ...', date: new Date(), etat_avancement: 'En cours de réparation'},
-  {reparation: 'Réparation ...', date: new Date(), etat_avancement: 'Réparée'},
-  {reparation: 'Réparation ...', date: new Date(), etat_avancement: 'En cours de réparation'},
-  {reparation: 'Réparation ...', date: new Date(), etat_avancement: 'Réparée'},
-  {reparation: 'Réparation ...', date: new Date(), etat_avancement: 'En cours de réparation'},
-  {reparation: 'Réparation ...', date: new Date(), etat_avancement: 'Réparée'},
-  {reparation: 'Réparation ...', date: new Date(), etat_avancement: 'En cours de réparation'},
+  {reparation: 'Réparation ...', etat_avancement: 'Réparée'},
+  {reparation: 'Réparation ...', etat_avancement: 'En cours de réparation'},
+  {reparation: 'Réparation ...', etat_avancement: 'Réparée'},
+  {reparation: 'Réparation ...', etat_avancement: 'En cours de réparation'},
+  {reparation: 'Réparation ...', etat_avancement: 'Réparée'},
+  {reparation: 'Réparation ...', etat_avancement: 'En cours de réparation'},
+  {reparation: 'Réparation ...', etat_avancement: 'Réparée'},
+  {reparation: 'Réparation ...', etat_avancement: 'En cours de réparation'},
+  {reparation: 'Réparation ...', etat_avancement: 'Réparée'},
+  {reparation: 'Réparation ...', etat_avancement: 'En cours de réparation'},
+  {reparation: 'Réparation ...', etat_avancement: 'Réparée'},
+  {reparation: 'Réparation ...', etat_avancement: 'En cours de réparation'},
+  {reparation: 'Réparation ...', etat_avancement: 'Réparée'},
+  {reparation: 'Réparation ...', etat_avancement: 'En cours de réparation'},
 ];
+
+interface AchatPiece {
+  nom: string,
+  pu: number,
+  quantite: number
+}
+
+interface Reparation {
+  achat_piece: AchatPiece[],
+  frais: number,
+  debut_reparation: Date | null,
+  fin_reparation: Date | null,
+  description: string,
+  payement: Date | null
+}
+
+interface Voiture {
+  marque: string,
+  numero: string,
+  reparation: Reparation[]
+}
 
 @Component({
   selector: 'historique-reparation',
@@ -27,9 +50,11 @@ const ELEMENT_DATA = [
   styleUrls: ['./historique-reparation.component.css']
 })
 export class HistoriqueReparationComponent implements AfterViewInit, OnInit {
-  @Input() liste_voiture!: any[];
-  displayedColumns: string[] = ['reparation', 'date', 'etat_avancement'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  isLoading = true;
+  error = false;
+  voiture!: Voiture;
+  displayedColumns: string[] = ['reparation', 'debut', 'fin', 'etat_avancement'];
+  dataSource!: MatTableDataSource<Reparation>;
   numero!: string;
   marque!: string;
 
@@ -38,14 +63,85 @@ export class HistoriqueReparationComponent implements AfterViewInit, OnInit {
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
       this.numero = params.get('numero')!;
+      this.getData();
     });
   }
 
-  @ViewChild('historiquereparation') inputElement!: ElementRef;
   @ViewChild(MatSort) sort!: MatSort;
+  getData() {
+    this.isLoading = true;
+    fetch('https://garage-backend-sigma.vercel.app/voiture/voiture-present', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-auth-token': sessionStorage.getItem('token-client')!
+      }
+    })
+    .then(response => {
+      const rep = response.json();
+      const code = response.status;
+      if(code == 200) {
+        rep.then(data => {
+
+          data.forEach((element:any) => {
+            if(element.numero == this.numero) {
+              let reparation : Reparation[] = [];
+              element.evenement.reparation.forEach((element2:any) => {
+                let achat_piece : AchatPiece[] = [];
+                element2.achat_piece.forEach((element3:any) => {
+                  const ap : AchatPiece = {
+                    nom : element3.nom,
+                    pu : element3.pu,
+                    quantite : element3.quantite
+                  }
+                  achat_piece.push(ap);
+                });
+                const rep : Reparation = {
+                  frais: element2.frais,
+                  debut_reparation: element2.debut_reparation,
+                  fin_reparation : element2.fin_reparation,
+                  description : element2.description,
+                  achat_piece : achat_piece,
+                  payement: element2.payement
+                };
+                reparation.push(rep);
+              });
+              const voiture : Voiture = {
+                marque: element.marque,
+                numero: element.numero,
+                reparation: reparation
+              }
+              this.voiture = voiture;
+            }
+
+          });
+          this.dataSource = new MatTableDataSource(this.voiture.reparation);
+
+          this.dataSource.sort = this.sort;
+          if(document.getElementById('historiquereparation')) document.getElementById('historiquereparation')!.scrollIntoView({behavior: 'smooth'});
+
+
+          this.isLoading = false;
+          this.error = false;
+        });
+      } else {
+        this.error = true;
+        console.error("Une erreur s'est produite");
+      }
+    })
+    .catch(error => {
+      this.isLoading = false;
+      this.error = true;
+      console.error(error);
+    })
+  }
+
+  @ViewChild('historiquereparation') inputElement!: ElementRef;
   ngAfterViewInit() {
-    this.inputElement.nativeElement.scrollIntoView({ behavior: 'smooth' });
-    this.dataSource.sort = this.sort;
+    if(!this.isLoading && !this.error) {
+      this.inputElement.nativeElement.scrollIntoView({ behavior: 'smooth' });
+      this.dataSource.sort = this.sort;
+    }
   }
 
   announceSortChange(sortState: Sort | any) {
